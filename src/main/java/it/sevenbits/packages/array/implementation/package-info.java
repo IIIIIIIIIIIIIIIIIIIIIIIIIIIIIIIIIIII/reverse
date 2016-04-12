08cb519b4e34ce178c6e753149a6b863ae500ef1
Grechkoseev <igorgrechkoseev@gmail.com>
2016-04-12 21:58:18 +0600

    corrected package-info file

--- src/main/java/it/sevenbits/packages/array/implementation/package-info.java
+++ src/main/java/it/sevenbits/packages/array/implementation/package-info.java
@@ -1,4 +1,4 @@
 /**
- * package has classes with methods of printing array and reversing array
+ * package has classes with methods of printing array and reversing array and content that contain the array
  */
 package it.sevenbits.packages.array.implementation;
